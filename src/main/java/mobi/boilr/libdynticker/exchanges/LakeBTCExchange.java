744c22980398b5b96aab3b8288874ed2fd35058f
dllud <david.ludovino@gmail.com>
2014-10-12 17:53:02 +0100

    Added LakeBTC exchange. Increased version number.

---
+++ src/main/java/mobi/boilr/libdynticker/exchanges/LakeBTCExchange.java
@@ -0,0 +1,45 @@
+package mobi.boilr.libdynticker.exchanges;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import mobi.boilr.libdynticker.core.Exchange;
+import mobi.boilr.libdynticker.core.Pair;
+
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.map.ObjectMapper;
+
+public class LakeBTCExchange extends Exchange {
+	private static final List<Pair> pairs;
+	static {
+		List<Pair> tempPairs = new ArrayList<Pair>();
+		tempPairs.add(new Pair("BTC", "USD"));
+		tempPairs.add(new Pair("BTC", "CNY"));
+		pairs = Collections.unmodifiableList(tempPairs);
+	}
+
+	public LakeBTCExchange(long experiedPeriod) {
+		super("LakeBTC", experiedPeriod);
+	}
+
+	@Override
+	protected List<Pair> getPairsFromAPI() throws IOException {
+		return pairs;
+	}
+
+	@Override
+	protected String getTicker(Pair pair) throws IOException {
+		if(!pairs.contains(pair))
+			throw new IOException("Invalid pair.");
+		JsonNode node = (new ObjectMapper()).readTree(new URL("https://www.lakebtc.com/api_v1/ticker"));
+		return parseJSON(node, pair);
+	}
+
+	@Override
+	public String parseJSON(JsonNode node, Pair pair) throws IOException {
+		return node.get(pair.getExchange()).get("last").toString();
+	}
+}
---
+++ src/test/java/mobi/boilr/libdynticker/exchanges/LakeBTCExchangeTest.java
@@ -0,0 +1,71 @@
+package mobi.boilr.libdynticker.exchanges;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import mobi.boilr.libdynticker.core.ExchangeTest;
+import mobi.boilr.libdynticker.core.Pair;
+
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LakeBTCExchangeTest extends ExchangeTest {
+	@Override
+	@Before
+	public void setUp() throws Exception {
+		testExchange = new LakeBTCExchange(1000);
+	}
+
+	@Override
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testParseJson() {
+		try {
+			Pair pair = new Pair("BTC", "USD");
+			JsonNode node = (new ObjectMapper().readTree(new File("src/test/json/lakebtc-ticker.json")));
+			String lastValue = testExchange.parseJSON(node, pair);
+			Assert.assertEquals("358.44", lastValue);
+		} catch(IOException e) {
+			Assert.fail();
+		}
+	}
+
+	@Test
+	public void testGetPairs() {
+		List<Pair> pairs;
+		try {
+			pairs = testExchange.getPairs();
+			Assert.assertTrue(pairs.contains(new Pair("BTC", "USD")));
+			Assert.assertTrue(pairs.contains(new Pair("BTC", "CNY")));
+			Assert.assertFalse(pairs.contains(new Pair("Invalid", "BTC")));
+		} catch(IOException e) {
+			Assert.fail();
+		}
+	}
+
+	@Test
+	public void testGetLastValue() {
+		try {
+			double lastValue = testExchange.getLastValue(new Pair("BTC", "USD"));
+			Assert.assertNotNull(lastValue);
+			lastValue = testExchange.getLastValue(new Pair("BTC", "CNY"));
+			Assert.assertNotNull(lastValue);
+		} catch(IOException e) {
+			Assert.fail();
+		}
+	}
+
+	@Test(expected = IOException.class)
+	public void testFailGetLastValue() throws NumberFormatException, IOException {
+		testExchange.getLastValue(new Pair("Invalid", "BTC"));
+	}
+
+}
