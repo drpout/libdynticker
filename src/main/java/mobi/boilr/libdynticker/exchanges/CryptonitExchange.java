1272bb0d64abbde091f8f7957d818c0fc11a6ff8
André Santos <andrefilipebrazsantos@gmail.com>
2014-10-15 14:55:11 +0100

    Cryptonit added.

---
+++ src/main/java/mobi/boilr/libdynticker/exchanges/CryptonitExchange.java
@@ -0,0 +1,57 @@
+package mobi.boilr.libdynticker.exchanges;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.TypeReference;
+
+import mobi.boilr.libdynticker.core.Exchange;
+import mobi.boilr.libdynticker.core.Pair;
+
+public class CryptonitExchange extends Exchange {
+
+	public CryptonitExchange(long experiedPeriod) {
+		super("Cryptonit", experiedPeriod);
+	}
+
+	@Override
+	protected List<Pair> getPairsFromAPI() throws IOException {
+		List<Pair> pairs = new ArrayList<Pair>();
+		String addr = "http://cryptonit.net/apiv2/rest/public/pairs.json";
+		URLConnection uc = new URL(addr).openConnection();
+		uc.addRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)");
+		uc.connect();
+		TypeReference<List<String[]>> typeReference = new TypeReference<List<String[]>>(){
+		};		
+		List<String[]> elements = new ObjectMapper().readValue((uc.getInputStream()),typeReference);
+		for(String[] node : elements){
+			pairs.add(new Pair(node[0],node[1]));
+		}
+		return pairs;
+	}
+
+	@Override
+	protected String getTicker(Pair pair) throws IOException {
+		String addr = "https://cryptonit.net/apiv2/rest/public/ccorder.json?bid_currency="+pair.getCoin()+"&ask_currency="+pair.getExchange()+"&rate=1";
+		URLConnection uc = new URL(addr).openConnection();
+		uc.addRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)");
+		uc.connect();
+		JsonNode node = new ObjectMapper().readTree(uc.getInputStream());
+		return parseJSON(node, pair);
+	}
+
+	@Override
+	public String parseJSON(JsonNode node, Pair pair) throws IOException {
+		String last = node.get(0).getTextValue();
+		if(last.equals("currency pair not available"))
+			throw new IOException(last);
+		else
+			return last;
+	}
+
+}
---
+++ src/test/java/mobi/boilr/libdynticker/exchanges/CryptonitExchangeTest.java
@@ -0,0 +1,58 @@
+package mobi.boilr.libdynticker.exchanges;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import mobi.boilr.libdynticker.core.ExchangeTest;
+import mobi.boilr.libdynticker.core.Pair;
+
+public class CryptonitExchangeTest extends ExchangeTest {
+
+	
+	
+	@Override
+	@Before
+	public void setUp() throws Exception {
+		testExchange = new CryptonitExchange(1000);
+	}
+
+	@Override
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testParseJson() {
+		try {
+			Pair pair = new Pair("BTC", "DOGE");
+			JsonNode node = (new ObjectMapper().readTree(new File("src/test/json/cryptonit-ticker.json")));
+			String lastValue = testExchange.parseJSON(node, pair);
+			Assert.assertEquals("401.781", lastValue);
+		} catch (IOException e) {
+			e.printStackTrace();
+			Assert.fail();
+		}
+	}
+
+	@Test
+	public void testGetPairs() {
+		List<Pair> pairs;
+		try {
+			pairs = testExchange.getPairs();
+			Assert.assertTrue(pairs.contains(new Pair("BTC", "LTC")));
+			Assert.assertTrue(pairs.contains(new Pair("EUR", "NMC")));
+			Assert.assertFalse(pairs.contains(new Pair("InvalidCoin", "BTC")));
+		} catch (IOException e) {
+			e.printStackTrace();
+			Assert.fail();
+		}
+	}
+}
