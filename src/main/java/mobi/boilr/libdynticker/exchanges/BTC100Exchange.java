2831052dcd6369353255c94a73a9f2d49af713e0
André Santos <andrefilipebrazsantos@gmail.com>
2014-10-17 13:12:18 +0100

    Added BTC100.

---
+++ src/main/java/mobi/boilr/libdynticker/exchanges/BTC100Exchange.java
@@ -0,0 +1,72 @@
+package mobi.boilr.libdynticker.exchanges;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import mobi.boilr.libdynticker.core.Exchange;
+import mobi.boilr.libdynticker.core.Pair;
+
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.TypeReference;
+
+public class BTC100Exchange extends Exchange {
+	private static final List<Pair> PAIRS;
+
+	private static final Pair BTC_CNY = new Pair("BTC", "CNY");
+
+	private static final String BTC_CNY_ID = "bit";
+
+	private static final Pair LTC_CNY = new Pair("LTC", "CNY");
+
+	private static final String LTC_CNY_ID = "tic";
+
+	private static final Pair DOGE_CNY = new Pair("DOGE", "CNY");
+
+	private static final String DOGE_CNY_ID = "dic";
+
+	static {
+		List<Pair> pairs = new ArrayList<Pair>();
+		pairs.add(BTC_CNY);
+		pairs.add(LTC_CNY);
+		pairs.add(DOGE_CNY);
+		PAIRS = Collections.unmodifiableList(pairs);
+	}
+
+	public BTC100Exchange(long experiedPeriod) {
+		super("BTC100", experiedPeriod);
+	}
+
+	@Override
+	protected List<Pair> getPairsFromAPI() throws IOException {
+		return PAIRS;
+	}
+
+	@Override
+	protected String getTicker(Pair pair) throws IOException {
+		JsonNode node = new ObjectMapper().readTree(new URL(
+				"https://www.btc100.com/apidata/getdata.json"));
+		return parseJSON(node, pair);
+	}
+
+	@Override
+	public String parseJSON(JsonNode node, Pair pair) throws IOException {
+		TypeReference<List<JsonNode>> typeRef = new TypeReference<List<JsonNode>>() {
+		};
+		List<JsonNode> json = new ObjectMapper().readValue(node, typeRef);
+		String id;
+		if (pair.equals(BTC_CNY))
+			id = BTC_CNY_ID;
+		else if (pair.equals(LTC_CNY))
+			id = LTC_CNY_ID;
+		else if (pair.equals(DOGE_CNY))
+			id = DOGE_CNY_ID;
+		else
+			throw new IOException(pair + " is invalid.");
+
+		return json.get(0).get(id).getTextValue();
+	}
+}
---
+++ src/test/java/mobi/boilr/libdynticker/exchanges/BTC100ExchangeTest.java
@@ -0,0 +1,65 @@
+package mobi.boilr.libdynticker.exchanges;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import mobi.boilr.libdynticker.core.ExchangeTest;
+import mobi.boilr.libdynticker.core.Pair;
+
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.JsonProcessingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BTC100ExchangeTest extends ExchangeTest {
+
+	@Override
+	@Before
+	public void setUp() throws Exception {
+		testExchange = new BTC100Exchange(10000);
+	}
+
+	@Override
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetPairs() {
+		List<Pair> pairs;
+		try {
+			pairs = testExchange.getPairs();
+			Assert.assertTrue(pairs.contains(new Pair("BTC", "CNY")));
+			Assert.assertTrue(pairs.contains(new Pair("LTC", "CNY")));
+			Assert.assertTrue(pairs.contains(new Pair("DOGE", "CNY")));
+			Assert.assertFalse(pairs.contains(new Pair("InvalidCoin", "BTC")));
+		}
+		catch (JsonProcessingException e) {
+			Assert.fail();
+			e.printStackTrace();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+			Assert.fail();
+		}
+	}
+
+	@Test
+	public void testParseJson() {
+		try {
+			Pair pair = new Pair("LTC", "CNY");
+			JsonNode node = (new ObjectMapper().readTree(new File(
+					"src/test/json/btc100-ticker.json")));
+			String lastValue = testExchange.parseJSON(node, pair);
+			Assert.assertEquals("24.440", lastValue);
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+			Assert.fail();
+		}
+	}
+}
