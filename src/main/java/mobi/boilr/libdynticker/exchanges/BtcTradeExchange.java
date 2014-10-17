e22f90ac48b434334fe7ef33dd57cb725eafde51
André Santos <andrefilipebrazsantos@gmail.com>
2014-10-17 13:30:12 +0100

    Added BtcTrade.

---
+++ src/main/java/mobi/boilr/libdynticker/exchanges/BtcTradeExchange.java
@@ -0,0 +1,48 @@
+package mobi.boilr.libdynticker.exchanges;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import mobi.boilr.libdynticker.core.Exchange;
+import mobi.boilr.libdynticker.core.Pair;
+
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.map.ObjectMapper;
+
+public class BtcTradeExchange extends Exchange {
+	private static final List<Pair> PAIRS;
+
+	static {
+		List<Pair> pairs = new ArrayList<Pair>();
+		pairs.add(new Pair("BTC", "CNY"));
+		PAIRS = Collections.unmodifiableList(pairs);
+	}
+
+	public BtcTradeExchange(long experiedPeriod) {
+		super("BtcTrade", experiedPeriod);
+
+	}
+
+	@Override
+	protected List<Pair> getPairsFromAPI() throws IOException {
+		return PAIRS;
+	}
+
+	@Override
+	protected String getTicker(Pair pair) throws IOException {
+		JsonNode node = new ObjectMapper().readTree(new URL(
+				"https://www.btctrade.com/api/ticker/"));
+		return parseJSON(node, pair);
+	}
+
+	@Override
+	public String parseJSON(JsonNode node, Pair pair) throws IOException {
+		if (PAIRS.contains(pair))
+			return node.get("last").toString();
+		else
+			throw new IOException(pair + " is invalid.");
+	}
+}
---
+++ src/main/java/mobi/boilr/libdynticker/exchanges/BtcTradeExchangeTest.java
@@ -0,0 +1,63 @@
+package mobi.boilr.libdynticker.exchanges;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import mobi.boilr.libdynticker.core.ExchangeTest;
+import mobi.boilr.libdynticker.core.Pair;
+
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.JsonProcessingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BtcTradeExchangeTest extends ExchangeTest {
+
+	@Override
+	@Before
+	public void setUp() throws Exception {
+		testExchange = new BtcTradeExchange(10000);
+	}
+
+	@Override
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetPairs() {
+		List<Pair> pairs;
+		try {
+			pairs = testExchange.getPairs();
+			Assert.assertTrue(pairs.contains(new Pair("BTC", "CNY")));
+			Assert.assertFalse(pairs.contains(new Pair("InvalidCoin", "BTC")));
+		}
+		catch (JsonProcessingException e) {
+			Assert.fail();
+			e.printStackTrace();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+			Assert.fail();
+		}
+	}
+
+	@Test
+	public void testParseJson() {
+		try {
+			Pair pair = new Pair("BTC", "CNY");
+			JsonNode node = (new ObjectMapper().readTree(new File(
+					"src/test/json/btctrade-ticker.json")));
+			String lastValue = testExchange.parseJSON(node, pair);
+			Assert.assertEquals("2318.15", lastValue);
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+			Assert.fail();
+		}
+	}
+}
