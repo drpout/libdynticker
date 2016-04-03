8dda6e521612c9ad750561cf9a1a56651ce56652
André Santos <andrefilipebrazsantos@gmail.com>
2016-04-03 12:06:30 +0100

    Testing pair no longer valid

--- src/test/java/mobi/boilr/libdynticker/exchanges/YobitExchangeTest.java
+++ src/test/java/mobi/boilr/libdynticker/exchanges/YobitExchangeTest.java
@@ -32,7 +32,7 @@ public class YobitExchangeTest extends ExchangeTest {
 		List<Pair> pairs;
 		try {
 			pairs = testExchange.getPairs();
-			Assert.assertTrue(pairs.contains(new Pair("LTC", "BTC")));
+			Assert.assertTrue(pairs.contains(new Pair("BTC", "USD")));
 			Assert.assertFalse(pairs.contains(new Pair("InvalidCoin", "BTC")));
 		}
 		catch(IOException e) {
