16a4754cc31cc6ab68ad67e89b15046bd2f27fda
dllud <dllud@riseup.net>
2017-07-27 11:17:18 +0100

    [task drpout/boilr#262] Added new pairs to Bitstamp.

--- src/main/java/mobi/boilr/libdynticker/exchanges/BitstampExchange.java
+++ src/main/java/mobi/boilr/libdynticker/exchanges/BitstampExchange.java
@@ -5,16 +5,24 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import org.codehaus.jackson.JsonNode;
+
 import mobi.boilr.libdynticker.core.Exchange;
 import mobi.boilr.libdynticker.core.Pair;
-
-import org.codehaus.jackson.JsonNode;
 
 public final class BitstampExchange extends Exchange {
 	private static final List<Pair> pairs;
 	static {
 		List<Pair> tempPairs = new ArrayList<Pair>();
 		tempPairs.add(new Pair("BTC", "USD"));
+		tempPairs.add(new Pair("BTC", "EUR"));
+		tempPairs.add(new Pair("EUR", "USD"));
+		tempPairs.add(new Pair("XRP", "USD"));
+		tempPairs.add(new Pair("XRP", "EUR"));
+		tempPairs.add(new Pair("XRP", "BTC"));
+		tempPairs.add(new Pair("LTC", "USD"));
+		tempPairs.add(new Pair("LTC", "EUR"));
+		tempPairs.add(new Pair("LTC", "BTC"));
 		pairs = Collections.unmodifiableList(tempPairs);
 	}
 
@@ -27,7 +35,8 @@ public final class BitstampExchange extends Exchange {
 		if(!pairs.contains(pair)) {
 			throw new IOException("Invalid pair: " + pair);
 		}
-		return parseTicker(readJsonFromUrl("https://www.bitstamp.net/api/ticker/"), pair);
+		return parseTicker(readJsonFromUrl("https://www.bitstamp.net/api/v2/ticker/" + pair.getCoin().toLowerCase()
+				+ pair.getExchange().toLowerCase()), pair);
 	}
 
 	@Override
--- src/test/java/mobi/boilr/libdynticker/exchanges/BitstampExchangeTest.java
+++ src/test/java/mobi/boilr/libdynticker/exchanges/BitstampExchangeTest.java
@@ -4,9 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-import mobi.boilr.libdynticker.core.ExchangeTest;
-import mobi.boilr.libdynticker.core.Pair;
-
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.After;
@@ -14,6 +11,9 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 //
+
+import mobi.boilr.libdynticker.core.ExchangeTest;
+import mobi.boilr.libdynticker.core.Pair;
 
 
 
@@ -30,12 +30,14 @@ public class BitstampExchangeTest extends ExchangeTest {
 	public void tearDown() throws Exception {
 	}
 
+	@Override
 	@Test
 	public void testGetPairs() {
 		List<Pair> pairs;
 		try {
 			pairs = testExchange.getPairs();
 			Assert.assertTrue(pairs.contains(new Pair("BTC", "USD")));
+			Assert.assertTrue(pairs.contains(new Pair("XRP", "USD")));
 			Assert.assertFalse(pairs.contains(new Pair("InvalidCoin", "BTC")));
 		}
 		catch(IOException e) {
@@ -51,7 +53,7 @@ public class BitstampExchangeTest extends ExchangeTest {
 			JsonNode node;
 			node = (new ObjectMapper().readTree(new File("src/test/json/bitstamp-ticker.json")));
 			String lastValue = testExchange.parseTicker(node, pair);
-			Assert.assertEquals("600.15", lastValue);
+			Assert.assertEquals("2583.92", lastValue);
 		}
 		catch(IOException e) {
 			Assert.fail();
